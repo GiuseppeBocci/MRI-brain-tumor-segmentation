bfbe252b84e0237be215f3f3b6cc11065118ea1b
Giuseppe Bocci <61885676+GiuseppeBocci@users.noreply.github.com>
2023-12-12 23:29:12 +0100

    Sagittal, Coronal, better threshold method

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clear all
 
 MRId = load('MRIdata.mat');
 
-slice = 77 ; % (90 esculsa mail tuomere termina tra 89e90) 89 - 64 (64 non si vede molto potrebbe essere esculsa)
+%slice = 77 ; % (90 esculsa mail tuomere termina tra 89e90) 89 - 64 (64 non si vede molto potrebbe essere esculsa)
 v = MRId.vol;
 %volumeViewer(v);
 
@@ -12,15 +12,19 @@ v = MRId.vol;
 %     imshow(v(:,:,s))
 % end
 
-%% ROI
+figure
+montage(v)
+title("MRI transversal")
+
+%% ROI T
 close all
 %I = v(:,:,slice);
 %[cropped, rect] = imcrop(I);
 rect = [133.5100  103.5100   51.9800   44.9800];
 
-[mask, thMask] = tumorMasks(v, rect, 65, 90);
+[mask, thMask] = tumorMasks(v, rect, 65, 90, 8);
 
-%% VOLUMES
+%% VOLUMES T
 close all
 
 pixelVol = MRId.pixdim(1)*MRId.pixdim(2)*MRId.pixdim(3) % mm^3
@@ -32,5 +36,56 @@ volT = pixelVol*sum(areaT)
 volumeViewer(mask)
 volumeViewer(thMask)
 
-%%
+%% SAGITTAL
+trasversalRot = flip(v, 3); % order of sicels
+% from rx to sx and not the inverse. No computational relevant
+trasversalRot = flip(trasversalRot, 1);
+sagittal = permute(trasversalRot, [3 2 1]);
+figure
+montage(sagittal)
+title("MRI sagittal")
+% slices 112(113) - 152(150)
+
+%% 
+close all
 volumeViewer close
+
+%I = sagittal(:,:,135);
+%[cropped, rect] = imcrop(I);
+rect = [137.5100   20.5100   42.9800   29.9800];
+
+[mask, thMask] = tumorMasks(sagittal, rect, 113, 150, 8); % threshold);
+
+[areaE, areaT] = areasFromMasks(mask, thMask);
+
+volE = pixelVol*sum(areaE)
+volT = pixelVol*sum(areaT)
+volumeViewer(mask)
+volumeViewer(thMask)
+
+%% CORONAL
+trasversalRot = flip(v, 3); % order of sicels
+% from front to back and not the inverse. No computational relevant
+trasversalRot = flip(trasversalRot, 2);
+coronal = permute(trasversalRot, [3 1 2]);
+figure
+montage(coronal)
+title("MRI coronal")
+% slices 81 - 116(115)
+
+%% 
+close all
+volumeViewer close
+
+% I = coronal(:,:,100);
+% [cropped, rect] = imcrop(I);
+rect = [104.5100   20.5100   42.9800   31.9800];
+
+[mask, thMask] = tumorMasks(coronal, rect, 81, 116, 8); % threshold);
+
+[areaE, areaT] = areasFromMasks(mask, thMask);
+
+volE = pixelVol*sum(areaE)
+volT = pixelVol*sum(areaT)
+volumeViewer(mask)
+volumeViewer(thMask)
--- tumorMasks.m
+++ tumorMasks.m
@@ -1,46 +1,65 @@
-function [maskE, maskTh] = tumorMasks(v, rect, sli, slf)
-    dimC = size(imcrop(v(:,:,1),rect));
+function [maskE, maskTh] = tumorMasks(vol, rect, sli, slf, ngraybits, varargin)
+    dimC = size(imcrop(vol(:,:,1),rect));
     
     roi = zeros(dimC(1), dimC(2), slf-sli+1, "uint8");
     lenS = slf-sli+1;
     for s = 1:lenS
+
+        I = imcrop(vol(:,:,s+sli-1),rect);
+
         % figure
-        I = imadjust(imcrop(v(:,:,s+sli-1),rect));
+        % subplot(1,3,1)
         % imshow(I)
-        % figure
-        % imshow(imadjust(I))
+        % subplot(1,3,3)
+        % bins = 2^ngraybits;
+        % histogram(I,bins)
+        % h = histcounts(I, bins);
+        % if s>lenS*0.1 && s<lenS*0.9 
+        %     I = imadjust(I);
+        % 
+        %     subplot(1,3,2)
+        %     imshow(I)
+        % end
+
         roi(:,:,s) = I;
         
         %figure
         %imshow(roi(:,:,s))
     end
+
+    figure
+    montage(roi)
+    title("ROI")
     
     %% Using Edge detection
-    
-    close all
     
     edges = zeros(dimC(1), dimC(2), slf-sli+1);
     for s = 1:lenS
         edges(:, :, s) = edge(roi(:,:,s) , 'log');
+        % figure
+        % imshow(edges(:,:,s))
     end
     
     
-    se90 = strel('line',3,90);
-    se0 = strel('line',3,0);
+    se90 = strel('line',3.4,90);
+    se0 = strel('line',3.4,0);
     seD = strel('diamond',1);
     
     mask = zeros(dimC(1), dimC(2), slf-sli+1);
     
     for s = 1:lenS
         idil = imdilate(edges(:,:,s),[se90 se0]);
+        
         ifill = imfill(idil);
+        % figure
+        % imshow(ifill)
         mf = imerode(ifill,seD);
         mf = imerode(mf,seD);
     
         mask(:,:,s) = mf;
     
-        %figure
-        %imshow(mask(:,:,s))
+        % figure
+        % imshow(mask(:,:,s))
     end 
     
     % for s = 1:lenS
@@ -48,57 +67,34 @@ function [maskE, maskTh] = tumorMasks(v, rect, sli, slf)
     %     imshow(labeloverlay(roi(:,:,s),mask(:,:,s)))
     % end
     
-    %% Using just threshold
+    %% Using threshold
     % Image adjustment is important here, in edges about nothing changes
-    
-    close all
-    
-    threshold = 130;
+    % For automatic threshold is better without imadjust to consider
+    % less of the fibrotic part of the brain
+    % For manual threshold imadjust is useful in same cases. Manual
+    % threshold suggested with imadjust is 150
+
+    if length(varargin) == 1 
+       threshold = cell2mat(varargin(1))/(2^ngraybits-1);
+    else
+        disp("Threshold automatically set");
+        threshold = graythresh(roi);
+    end
     disp("Threshold used in TH masking: "+string(threshold));
     
-    I = roi(:,:,10);
-    figure
-    subplot(1,2,1)
-    imshow(I)
-    subplot(1,2,2)
-    histogram(I)
+    thMask = imbinarize(roi, threshold);
     
-    seD = strel('diamond',1);
-    
-    thMask = zeros(dimC(1), dimC(2), slf-sli+1, "double");
-    for s = 1:lenS
-        I = roi(:,:,s);
-        for i = 1:dimC(1)
-            for j = 1:dimC(2)
-                thMask(i,j, s) = I(i,j) >= threshold;    
-            end
-        end
-        
-        % figure
-        % imshow(255*I)
-    end
-    
-    thMask = 255*thMask;
-    for s = 1:lenS
-        thMask(:,:,s) = imerode(imfill(thMask(:,:,s)), seD);
-        % figure
-        % imshow(labeloverlay(roi(:,:,s),thMask(:,:,s)))
-    end
-    
-    
-    % for s = sli:slf
-    %     figure
-    %     imshow(logical(roi(:,:,s)))
+    % thMask = 255*thMask; % imfill needs a numerical matrix
+    % thMask= imfill(thMask); improves manual threshold
+
+    % for s = 1:lenS
+    %     % figure
+    %     % imshow(labeloverlay(roi(:,:,s),thMask(:,:,s)))
     % end
+
     %%
     maskE = mask;
     maskTh = thMask;
 
-    %%
-    % figure
-    % montage(roi, colormap("gray"));
-    % hold on
-    % montage(mask, [0 0 1; 0 1 0]);
-    % drawnow;
 end
 
