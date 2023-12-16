36e05d06fef951d178d49b2964f6e8dcc9fe7c05
Giuseppe Bocci <61885676+GiuseppeBocci@users.noreply.github.com>
2023-12-16 17:07:51 +0100

    Add files via upload

--- main.m
+++ main.m
@@ -12,22 +12,38 @@ v = MRId.vol;
 %     imshow(v(:,:,s))
 % end
 
+pixelVol = MRId.pixdim(1)*MRId.pixdim(2)*MRId.pixdim(3); % mm^3
+garylevelsbits = 8;
+
+%%
+%Gaussian noise
+v = imnoise(MRId.vol, 'gaussian', 0, 1e-3);
+%v = imnoise(MRId.vol, 'gaussian', 0, 0.1);
+%v = imnoise(MRId.vol, 'gaussian', 0.5, 1e-3); 
+% v = imnoise(MRId.vol, 'gaussian', -0.5, 1e-3); 
+% v = 255-v;
+
+%Salt and pepper
+v = imnoise(v, 'salt & pepper', 0.05);
+%v = imnoise(MRId.vol, 'salt & pepper', 0.2);
+% SP3 = imnoise(v_ax, 'salt & pepper', 0.35);
+
+%% ROI T
+close all
+volumeViewer close
+
 figure
 montage(v)
 title("MRI transversal")
 
-%% ROI T
-close all
 %I = v(:,:,slice);
 %[cropped, rect] = imcrop(I);
 rect = [133.5100  103.5100   51.9800   44.9800];
 
-[mask, thMask] = tumorMasks(v, rect, 65, 90, 8);
+[mask, thMask] = tumorMasks(v, rect, 65, 90, garylevelsbits);
 
 %% VOLUMES T
 close all
-
-pixelVol = MRId.pixdim(1)*MRId.pixdim(2)*MRId.pixdim(3) % mm^3
 
 [areaE, areaT] = areasFromMasks(mask, thMask);
 
@@ -37,6 +53,7 @@ volumeViewer(mask)
 volumeViewer(thMask)
 
 %% SAGITTAL
+% No computational rilevant
 trasversalRot = flip(v, 3); % order of sicels
 % from rx to sx and not the inverse. No computational relevant
 trasversalRot = flip(trasversalRot, 1);
@@ -54,19 +71,22 @@ volumeViewer close
 %[cropped, rect] = imcrop(I);
 rect = [137.5100   20.5100   42.9800   29.9800];
 
-[mask, thMask] = tumorMasks(sagittal, rect, 113, 150, 8); % threshold);
+[mask, thMask] = tumorMasks(sagittal, rect, 113, 150, garylevelsbits); % threshold);
 
 [areaE, areaT] = areasFromMasks(mask, thMask);
 
 volE = pixelVol*sum(areaE)
 volT = pixelVol*sum(areaT)
-volumeViewer(mask)
-volumeViewer(thMask)
+%volumeViewer(mask)
+%volumeViewer(thMask)
 
 %% CORONAL
+% No computational rilevant
 trasversalRot = flip(v, 3); % order of sicels
 % from front to back and not the inverse. No computational relevant
 trasversalRot = flip(trasversalRot, 2);
+% sx and dx not inverted. No computational relevant
+trasversalRot = flip(trasversalRot, 1);
 coronal = permute(trasversalRot, [3 1 2]);
 figure
 montage(coronal)
@@ -77,15 +97,15 @@ title("MRI coronal")
 close all
 volumeViewer close
 
-% I = coronal(:,:,100);
-% [cropped, rect] = imcrop(I);
-rect = [104.5100   20.5100   42.9800   31.9800];
+%I = coronal(:,:,100);
+%[cropped, rect] = imcrop(I);
+rect = [109.5100   21.5100   42.9800   29.9800];
 
-[mask, thMask] = tumorMasks(coronal, rect, 81, 116, 8); % threshold);
+[mask, thMask] = tumorMasks(coronal, rect, 81, 116, garylevelsbits); % threshold);
 
 [areaE, areaT] = areasFromMasks(mask, thMask);
 
 volE = pixelVol*sum(areaE)
 volT = pixelVol*sum(areaT)
-volumeViewer(mask)
-volumeViewer(thMask)
+%volumeViewer(mask)
+%volumeViewer(thMask)
--- tumorMasks.m
+++ tumorMasks.m
@@ -31,11 +31,116 @@ function [maskE, maskTh] = tumorMasks(vol, rect, sli, slf, ngraybits, varargin)
     montage(roi)
     title("ROI")
     
+    % roi = histeq(roi);
+    % figure
+    % montage(roi)
+    % title("ROI")
+
+    % for s=1:lenS
+    %     roi(:,:,s) = imadjust(roi(:,:,s));
+    % end
+    % figure
+    % montage(roi)
+    % title("ROI")
+    % figure
+    % for s = 1:lenS
+    %     subplot(6, 7, s)
+    %     imhist(roi(:,:,s), 256)
+    % end
+    % sgtitle('Histogram ROI original')
+    
+    %%
+    se90 = strel('line',3,90);
+    se0 = strel('line',3,0);
+    seD = strel('diamond',1);
+    h = [0 -1 0; -1 4 -1; 0 -1 0];
+    edges2 = zeros(dimC(1), dimC(2), slf-sli+1);
+    roi2 = roi/2;
+    for s = 1:lenS
+        
+        for i=1:dimC(1)
+            for j=1:dimC(2)
+                if roi2(i,j,s) > 75
+                    roi2(i,j,s) = roi2(i,j,s)+127;
+                    if(roi2(i,j,s) > 230)
+                        roi2(i,j,s) = 0;
+                    end
+                else
+                    %roi2(i,j,s) = 0;
+                end
+            end
+        end
+     
+    
+
+        
+
+        
+        
+    end
+    
+    
+    
+    figure
+    for s = 1:lenS
+        subplot(6,7,s)
+        imshow(roi2(:,:,s))
+    end
+    
+    roi3 = roi2;
+    dim_med = 3;
+    for s = 1:lenS
+        roi3(:,:,s) = medfilt2(roi3(:,:,s), [dim_med dim_med]);
+    end
+    figure
+    for s = 1:lenS
+        subplot(6,7,s)
+        imshow(roi3(:,:,s))
+    end
+    sgtitle("Solo avg");
+
+
+    figure
+    for s = 1:lenS
+        roi2(:,:,s) = imfill(roi2(:,:,s));
+        subplot(6,7,s)
+        imshow(roi2(:,:,s))
+    end
+
+
+    dim_med = 3;
+    for s = 1:lenS
+        roi2(:,:,s) = medfilt2(roi2(:,:,s), [dim_med dim_med]);
+    end
+
+    figure
+    for s = 1:lenS
+        subplot(6,7,s)
+        imshow(roi2(:,:,s))
+    end
+    figure
+    for s=1:lenS
+        edges2(:,:,s) = edge(roi2(:,:,s) , 'log');
+        
+        edges2(:,:,s) = imdilate(edges2(:,:,s),[se90 se0]);
+        subplot(6,7,s)
+        imshow(edges2(:,:,s))
+        
+        ifill = imfill(edges2(:,:,s));
+        %mf = imerode(ifill,seD);
+        edges2(:,:,s) = imerode(ifill,seD);
+
+        
+    end
+   
+
+
+
     %% Using Edge detection
     
     edges = zeros(dimC(1), dimC(2), slf-sli+1);
     for s = 1:lenS
-        edges(:, :, s) = edge(roi(:,:,s) , 'log');
+        edges(:, :, s) = edge(roi(:,:,s) , 'canny'); %'log');
         % figure
         % imshow(edges(:,:,s))
     end
@@ -48,7 +153,7 @@ function [maskE, maskTh] = tumorMasks(vol, rect, sli, slf, ngraybits, varargin)
     mask = zeros(dimC(1), dimC(2), slf-sli+1);
     
     for s = 1:lenS
-        idil = imdilate(edges(:,:,s),[se90 se0]);
+        idil = imdilate(edges2(:,:,s),[se90 se0]);
         
         ifill = imfill(idil);
         % figure
@@ -61,19 +166,18 @@ function [maskE, maskTh] = tumorMasks(vol, rect, sli, slf, ngraybits, varargin)
         % figure
         % imshow(mask(:,:,s))
     end 
-    
-    % for s = 1:lenS
-    %     figure
-    %     imshow(labeloverlay(roi(:,:,s),mask(:,:,s)))
-    % end
-    
+    figure
+    for s = 1:lenS
+        subplot(6,7,s)
+        imshow(labeloverlay(roi2(:,:,s),mask(:,:,s)))
+    end
     %% Using threshold
     % Image adjustment is important here, in edges about nothing changes
     % For automatic threshold is better without imadjust to consider
     % less of the fibrotic part of the brain
     % For manual threshold imadjust is useful in same cases. Manual
     % threshold suggested with imadjust is 150
-
+    roi = roi2; %TODO
     if length(varargin) == 1 
        threshold = cell2mat(varargin(1))/(2^ngraybits-1);
     else
@@ -83,6 +187,8 @@ function [maskE, maskTh] = tumorMasks(vol, rect, sli, slf, ngraybits, varargin)
     disp("Threshold used in TH masking: "+string(threshold));
     
     thMask = imbinarize(roi, threshold);
+
+    
     
     % thMask = 255*thMask; % imfill needs a numerical matrix
     % thMask= imfill(thMask); improves manual threshold
@@ -93,6 +199,11 @@ function [maskE, maskTh] = tumorMasks(vol, rect, sli, slf, ngraybits, varargin)
     % end
 
     %%
+    figure
+    for s = 1:lenS
+        subplot(6,7,s)
+        imshow(thMask(:,:,s))
+    end
     maskE = mask;
     maskTh = thMask;
 
